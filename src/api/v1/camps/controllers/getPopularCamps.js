1dbc1ebf5e9989712157e2f20747d03292186786
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2023-11-30 23:20:53 +0600

    Remove unnecessary console

--- src/api/v1/camps/controllers/getPopularCamps.js
+++ src/api/v1/camps/controllers/getPopularCamps.js
@@ -2,7 +2,7 @@ const Camp = require("../../../../models/camps/campSchema");
 
 const getPopularCamps = async (req, res) => {
   const camps = (await Camp.find()).splice(0, 6);
-  console.log(camps);
+  // console.log(camps);
   res.send(camps);
 };
 
