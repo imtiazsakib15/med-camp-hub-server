3b1e0274315a4fadf6d02cf4e4429b04e52d258a
imtiazsakib15 <sakib.imtiaz15@gmail.com>
2024-04-04 23:31:10 +0600

    Subscribe newslewtter api created

--- src/routes/v1/subscribeNewsletter/index.js
+++ src/routes/v1/subscribeNewsletter/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
+const getSubscribeNewsletter = require("../../../api/v1/subscribeNewsletters/controllers/getSubscribeNewsletter");
 const postSubscribeNewsletter = require("../../../api/v1/subscribeNewsletters/controllers/postSubscribeNewsletter");
+
+// Post a newsletter subscriber info to database
+router.get("/subscribe-newsletter/:email", getSubscribeNewsletter);
 
 // Post a newsletter subscriber info to database
 router.post("/subscribe-newsletter", postSubscribeNewsletter);
